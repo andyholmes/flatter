f772b0c7464881da4c550b0953761819def42970
Andy Holmes <andrew.g.r.holmes@gmail.com>
2025-03-21 10:51:41 -0700

    test: refactor the test app as a GTK app to exercise xwfb-run
    To ensure more of the test infrastrucure is being exercised,
    refactor the test application as a simply GTK app so that xvfw-run
    is actually used.

--- tests/flatpak/main.c
+++ tests/flatpak/main.c
@@ -3,10 +3,40 @@
 
 #include <stdlib.h>
 
+#include <gio/gio.h>
+#include <gtk/gtk.h>
+
 int
 main (int   argc,
       char *argv[])
 {
+  GMainLoop *loop;
+  GtkWindow *window;
+
+  gtk_init ();
+
+  loop = g_main_loop_new (NULL, FALSE);
+  window = g_object_new (GTK_TYPE_WINDOW,
+                         "title", "Flatter",
+                         "child", g_object_new (GTK_TYPE_LABEL,
+                                                "label",      "This is a test application for "
+                                                              "<a href='https://flatter.andyholmes.ca'>Flatter</a>.",
+                                                "use-markup", TRUE,
+                                                NULL),
+                         "default-height", 480,
+                         "default-width",  600,
+                         NULL);
+  g_signal_connect_swapped (window,
+                            "destroy",
+                            G_CALLBACK (g_main_loop_quit),
+                            loop);
+  gtk_window_present (window);
+#ifdef FLATTER_TEST
+  g_timeout_add_seconds_once (1, (GSourceOnceFunc)gtk_window_destroy, window);
+#endif
+  g_main_loop_run (loop);
+  g_main_loop_unref (loop);
+
 #ifdef FLATTER_FAILURE
   return EXIT_FAILURE;
 #else
